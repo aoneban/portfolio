bfa3f18340b569176dd71a9484d8b1021fdfa27e
aoneban <torgregion1@gmail.com>
2024-09-15 22:04:36 +0200

    feat: add sticky navbar to the project

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-import './style.css'
-import powerLoader from './modules/loader.js'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import powerLoader from './modules/loader.js';
+import scrollFunction from './modules/scrollheader.js';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';
 
 powerLoader();
+window.onscroll = function () {
+  scrollFunction();
+};
 
-/* document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter')) */
 
--- modules/loader.js
+++ modules/loader.js
@@ -1,12 +1,17 @@
 export default function powerLoader() {
-    window.addEventListener('load', function () {
-      setTimeout(function () {
-        var preloader = document.querySelector('.loader');
-        preloader.style.display = 'none';
-    
-        document.getElementById('header').style.display = 'block';
-        document.getElementById('main').style.display = 'block';
-        document.getElementById('footer').style.display = 'block';
-      }, 1000); 
-    });
-  }
+  window.addEventListener('load', function () {
+    setTimeout(function () {
+      var preloader = document.querySelector('.loader');
+      preloader.style.display = 'none';
+
+      document.getElementById('header').style.display = 'block';
+      document.getElementById('main').style.display = 'block';
+      document.getElementById('footer').style.display = 'block';
+      deleteLoader();
+    }, 1000);
+  });
+}
+
+function deleteLoader() {
+  document.querySelector('.preloader').remove();
+}

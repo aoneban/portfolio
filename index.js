ab7142c098304dd4d03c9d7e10b7e7231d207d42
aoneban <torgregion1@gmail.com>
2024-09-18 20:55:10 +0200

    feat: add animation to email-jumping

--- index.js
+++ index.js
@@ -27,4 +27,22 @@ document.addEventListener('DOMContentLoaded', () => {
   interObserver3();
   observerFirstGallery();
   observerSecondGallery();
+  emailDancing("hello@exgrid",'.jump-email-first');
+  emailDancing("info.com", '.jump-email-second')
 });
+
+function emailDancing(elem1, elem2) {
+  const text = elem1;
+  const textContainer = document.querySelector(elem2);
+  const arr = text.split('');
+  
+  arr.forEach((char, index) => {
+    const span = document.createElement('span');
+    span.classList.add('bouncing-letter');
+
+    span.textContent = char
+    textContainer.appendChild(span);
+    
+    span.style.animationDelay = `${index * 0.05}s`;
+  });
+}

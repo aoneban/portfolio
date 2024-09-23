1a366764cb7896d3ee53277e62e8026252544d54
aoneban <torgregion1@gmail.com>
2024-09-23 12:59:51 +0200

    fix: delete some console.logs

--- contact/currentpage.js
+++ contact/currentpage.js
@@ -1,6 +1,5 @@
 export default function currentPage() {
     const currentPage = window.location.pathname;
-    console.log(currentPage);
   
     const navLinks = document.querySelectorAll('.nav-link');
     navLinks.forEach((link) => {

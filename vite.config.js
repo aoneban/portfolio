d0da1aeb55fa79dc5e3520ff95292f7355e8fa83
aoneban <torgregion1@gmail.com>
2024-09-24 14:09:45 +0200

    fix: fix some little bugs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', 
   build: {
     rollupOptions: {
       input: {
         main: 'index.html',   
         about: 'about.html',
         contact: 'contact.html'
-      }
-    }
-  }
+      },
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]', // Настройка имен ассетов
+      },
+    },
+  },
 });

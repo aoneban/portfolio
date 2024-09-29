173c6c0197d5a846ff9752029ed79359aef404ae
aoneban <torgregion1@gmail.com>
2024-09-30 00:40:27 +0200

    feat: add animation to the experience block

--- src/about.js
+++ src/about.js
@@ -18,8 +18,27 @@ document.addEventListener('DOMContentLoaded', () => {
   emailDancing('info.com', '.jump-email-second');
   currentPage();
   openNav();
-  showContact("About\u00A0Me", 'contact')
+  showContact('About\u00A0Me', 'contact');
   scrollControl();
   launchCursorMoving('.cursor', -16, -16, 0.2);
   launchCursorMoving('.circle', -16, -16, 0.1);
+});
+
+const switcher = document.getElementById('switcher');
+switcher.addEventListener('click', () => {
+  const ex = document.querySelectorAll('.expir');
+  ex.forEach((elem) => {
+    if (!elem.classList.contains('hidden')) {
+      elem.classList.add('hidden');
+      elem.classList.remove('newclass');
+      document.querySelector('.exps').classList.remove('col');
+      document.querySelector('.edcn').classList.add('col');
+      document.querySelector('.edcn').classList.remove('col2');
+    } else {
+      elem.classList.remove('hidden');
+      elem.classList.add('newclass');
+      document.querySelector('.exps').classList.add('col');
+      document.querySelector('.edcn').classList.remove('col');
+    }
+  });
 });
